b064801977e98fbabc0c27f65e7e1f8df886dabc
zhaoyu_zhao <zhaoyu_zhao@aiguang.com>
2022-01-19 14:52:08 +0800

    2022-01-19

---
+++ src/main/java/ContainsNearbyDuplicate.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsNearbyDuplicate {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
