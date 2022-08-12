b86ae2a6eb5f479e544d629e42b5b0061366eea2
zhaoyu_zhao <zhaoyu_zhao@aiguang.com>
2022-08-12 21:28:37 +0800

    20220812

---
+++ src/main/java/GroupThePeople.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupThePeople {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3,3,3,3,3,1,3};
+        System.out.println(groupThePeople(arr));
+    }
+
+    public static List<List<Integer>> groupThePeople(int[] groupSizes) {
+        Map<Integer, List<Integer>> groups = new HashMap<Integer, List<Integer>>();
+        int n = groupSizes.length;
+        for (int i = 0; i < n; i++) {
+            int size = groupSizes[i];
+            groups.putIfAbsent(size, new ArrayList<Integer>());
+            groups.get(size).add(i);
+        }
+        List<List<Integer>> groupList = new ArrayList<List<Integer>>();
+        for (Map.Entry<Integer, List<Integer>> entry : groups.entrySet()) {
+            int size = entry.getKey();
+            List<Integer> people = entry.getValue();
+            int groupCount = people.size() / size;
+            for (int i = 0; i < groupCount; i++) {
+                List<Integer> group = new ArrayList<Integer>();
+                int start = i * size;
+                for (int j = 0; j < size; j++) {
+                    group.add(people.get(start + j));
+                }
+                groupList.add(group);
+            }
+        }
+        return groupList;
+    }
+}
