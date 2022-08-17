fba7726546a431003b7559518848f6c3c93b5b28
zhaoyu_zhao <zhaoyu_zhao@aiguang.com>
2022-08-17 15:03:53 +0800

    20220817

---
+++ src/main/java/model/TreeNode.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class TreeNode {
+    public int val = 0;
+
+    public TreeNode left = null;
+
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+
+        this.val = val;
+
+    }
+}
